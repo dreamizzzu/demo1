5960a6547af6512ccc44f11a4a214f3b9a261bfd
dreamizzzu <15784948+dreamizzzu@user.noreply.gitee.com>
2026-03-13 00:05:43 +0800

    3.13第一次提交

---
+++ src/main/java/com/example/demo1/common/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.demo1.common;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public Result<?> handleException(Exception e) {
+        e.printStackTrace();
+        return Result.error(500, "服务器异常：" + e.getMessage());
+    }
+
+}
